c474ffe070009581f945002a5695eb8b33566fb5
SeyyedAhmadRokni <ahmadr1381@gmail.com>
2023-10-31 22:40:25 +0330

    udp connection successd

---
+++ udp_cli.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <strings.h>
+#include "server.c"
+
+const int SIZE = 1024;
+int main(int argc, char **argv){
+    
+    char *ip = "127.0.0.1";
+    int port = 8080;
+
+    int sockfd;
+    struct sockaddr_in addr;
+    char buffer[1024];
+    socklen_t addr_size;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&addr, '\0', sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
+
+    bzero(buffer, 1024);
+    strcpy(buffer, "Hello man\n");
+
+    int x = sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)& addr, sizeof(addr));
+
+    bzero(buffer, 1024);
+    sprintf(buffer, "code of send : %d \n", x);
+    write(1, buffer, strlen(buffer));
+    addr_size = sizeof(addr);
+    recvfrom(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&addr, &addr_size);
+    write(1, buffer, strlen(buffer));
+
+
+}
---
+++ udp_server.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <strings.h>
+
+int main(int argc, char **argv){
+    
+    char *ip = "127.0.0.1";
+    int port = 8080;
+    int n;
+    char buffer[1024];
+    memset(buffer, '\0', 1024);
+
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sockfd < 0){
+        char *err_msg = "socket not created";
+        write(2, err_msg, strlen(err_msg));
+    }
+
+    memset(&server_addr, '\0', sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    n = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (n<0){
+        char *err_msg = "bind errored";
+        write(2, err_msg, strlen(err_msg));
+    }
+
+    bzero(buffer, 1024);
+    addr_size = sizeof(client_addr);
+    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
+    write(1, buffer, strlen(buffer));
+    bzero(buffer, 1024);
+    sprintf(buffer, "Wellcome to UDP");
+    sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
+
+}
